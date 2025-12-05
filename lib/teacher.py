afec5da9ddcaaeb44a526bd1048d5ff7157c0367
Tommymuia <muiathomas.mt@gmail.com>
2025-12-05 11:52:39 +0300

    added

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,14 @@
-#!/usr/bin/env python
-
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        # Predefined knowledge list
+        self.knowledge = [
+            "Math", "Science", "History", "Art", "Physical Education"
+        ]
 
     def teach(self):
-        pass
+        # Returns a random piece of knowledge
+        return random.choice(self.knowledge)
